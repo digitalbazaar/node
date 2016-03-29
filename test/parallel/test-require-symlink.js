71bd0c5c051e6f6ae61c7870f77e4299c7dd68c3
Alex Lamar <alamar@digitalbazaar.com>
2016-03-29 13:54:00 -0400

    Cache symlinked modules according to their absolute path.

--- test/parallel/test-require-symlink.js
+++ test/parallel/test-require-symlink.js
@@ -6,23 +6,36 @@ var fs = require('fs');
 
 var module = require('module');
 
-const linkTarget = path.join(common.fixturesDir,
-  '/module-require-symlink/node_modules/dep2/');
-const linkDir = path.join(common.fixturesDir,
-  '/module-require-symlink/node_modules/dep1/node_modules/dep2');
+const linkTarget1 = path.join(common.fixturesDir,
+  '/module-require-symlink/directory1/node_modules/dep2/');
+const linkDir1 = path.join(common.fixturesDir,
+  '/module-require-symlink/directory1/node_modules/dep1/node_modules/dep2');
+const linkTarget2 = path.join(common.fixturesDir,
+  '/module-require-symlink/directory1/node_modules/dep1/node_modules/bar/');
+const linkDir2 = path.join(common.fixturesDir,
+  '/module-require-symlink/directory2/node_modules/bar');
+
+
+process.on('exit', function() {
+  fs.unlinkSync(linkDir1);
+  fs.unlinkSync(linkDir2);
+});
 
 if (common.isWindows) {
   console.log('Skipped: windows non-applicable');
   return;
 } else {
-  fs.symlinkSync(linkTarget, linkDir);
+  fs.symlinkSync(linkTarget1, linkDir1);
+  fs.symlinkSync(linkTarget2, linkDir2);
 }
 
-var fooModule = require(common.fixturesDir + '/module-require-symlink/foo.js');
-
+var fooModule = require(common.fixturesDir +
+  '/module-require-symlink/directory1/foo.js');
+var bazModule = require(common.fixturesDir +
+  '/module-require-symlink/directory2/baz.js');
 assert.equal(fooModule.dep1.bar.version, 'CORRECT_VERSION');
 assert.equal(fooModule.dep2.bar.version, 'CORRECT_VERSION');
-
-process.on('exit', function() {
-  fs.unlinkSync(linkDir);
-});
+assert.equal(bazModule.bar.version, 'CORRECT_VERSION');
+// Assert same module was not loaded twice
+assert.equal(fooModule.dep1.bar.timestamp, fooModule.dep2.bar.timestamp);
+assert.equal(fooModule.dep1.bar.timestamp, bazModule.bar.timestamp);
