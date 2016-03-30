30288b11ce150c18d2285033ef3c4d72e6cf2893
Alex Lamar <alawi@vt.edu>
2016-03-29 23:28:35 -0400

    module: preserve symlinks when requiring
    Currently, required modules use the real location of the
    package/file as their __filename and __dirname, instead
    of the symlinked path if it exists. This behaviour is
    undocumented (it even goes against documentation in
    certain scenarios), creating hard-to-debug problems
    for developers who wish to leverage filesystem abstractions
    to lay out their application.
    
    This patch resolves all required modules to their canonical
    path while still preserving any symlinks within the path,
    instead of resolving to their canonical realpath. The one
    special case observed is when the main module is loaded
    -- in this case, the realpath does need to be used
    in order for the main module to load properly.

---
+++ test/parallel/test-require-symlink.js
@@ -0,0 +1,41 @@
+'use strict';
+var common = require('../common');
+var assert = require('assert');
+var path = require('path');
+var fs = require('fs');
+var exec = require('child_process').exec;
+
+const linkTarget = path.join(common.fixturesDir,
+  '/module-require-symlink/node_modules/dep2/');
+
+const linkDir = path.join(common.fixturesDir,
+  '/module-require-symlink/node_modules/dep1/node_modules/dep2');
+
+if (common.isWindows) {
+  // On Windows, creating symlinks requires admin privileges.
+  // We'll only try to run symlink test if we have enough privileges.
+  exec('whoami /priv', function(err, o) {
+    if (err || o.indexOf('SeCreateSymbolicLinkPrivilege') == -1) {
+      console.log('Skipped: insufficient privileges');
+      return;
+    } else {
+      test();
+    }
+  });
+} else {
+  test();
+}
+
+function test() {
+  process.on('exit', function() {
+    fs.unlinkSync(linkDir);
+  });
+
+  fs.symlinkSync(linkTarget, linkDir);
+
+  var fooModule =
+    require(common.fixturesDir + '/module-require-symlink/foo.js');
+
+  assert.equal(fooModule.dep1.bar.version, 'CORRECT_VERSION');
+  assert.equal(fooModule.dep2.bar.version, 'CORRECT_VERSION');
+}
