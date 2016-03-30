ba735a08d8b20e15114443b3b04f4ae0ce4e48f0
Alex Lamar <alawi@vt.edu>
2016-03-29 20:01:20 -0400

    Add case for windows test.

--- test/parallel/test-require-symlink.js
+++ test/parallel/test-require-symlink.js
@@ -3,26 +3,38 @@ var common = require('../common');
 var assert = require('assert');
 var path = require('path');
 var fs = require('fs');
-
-var module = require('module');
+var exec = require('child_process').exec;
 
 const linkTarget = path.join(common.fixturesDir,
   '/module-require-symlink/node_modules/dep2/');
+
 const linkDir = path.join(common.fixturesDir,
   '/module-require-symlink/node_modules/dep1/node_modules/dep2');
 
 if (common.isWindows) {
-  console.log('Skipped: windows non-applicable');
-  return;
+  // On Windows, creating symlinks requires admin privileges.
+  // We'll only try to run symlink test if we have enough privileges.
+  exec('whoami /priv', function(err, o) {
+    if (err || o.indexOf('SeCreateSymbolicLinkPrivilege') == -1) {
+      console.log('Skipped: insufficient privileges');
+      return;
+    } else {
+      test();
+    }
+  });
 } else {
-  fs.symlinkSync(linkTarget, linkDir);
+  test();
 }
 
-var fooModule = require(common.fixturesDir + '/module-require-symlink/foo.js');
+function test() {
+  process.on('exit', function () {
+    fs.unlinkSync(linkDir);
+  });
 
-assert.equal(fooModule.dep1.bar.version, 'CORRECT_VERSION');
-assert.equal(fooModule.dep2.bar.version, 'CORRECT_VERSION');
+  fs.symlinkSync(linkTarget, linkDir);
 
-process.on('exit', function() {
-  fs.unlinkSync(linkDir);
-});
+  var fooModule = require(common.fixturesDir + '/module-require-symlink/foo.js');
+
+  assert.equal(fooModule.dep1.bar.version, 'CORRECT_VERSION');
+  assert.equal(fooModule.dep2.bar.version, 'CORRECT_VERSION');
+}
